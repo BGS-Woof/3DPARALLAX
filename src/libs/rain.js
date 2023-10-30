4bbfa66bf1c17c9051e6d6510f1995227521fbbe
Woof <Pablotoopchik@gmail.com>
2023-10-30 21:55:42 +0600

    parallax

---
+++ src/libs/rain.js
@@ -0,0 +1,71 @@
+const canvas = document.querySelector(".rain");
+const c = canvas.getContext("2d");
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+function randomNum(max, min) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function RainDrops(x, y, endy, velocity, opacity) {
+  this.x = x;
+  this.y = y;
+  this.endy = endy;
+  this.velocity = velocity;
+  this.opacity = opacity;
+
+  this.draw = function () {
+    c.beginPath();
+    c.moveTo(this.x, this.y);
+    c.lineTo(this.x, this.y - this.endy);
+    c.lineWidth = 1;
+    c.strokeStyle = "rgba(255, 255, 255, " + this.opacity + ")";
+    c.stroke();
+  };
+
+  this.update = function () {
+    const rainEnd = canvas.height + 100;
+    if (this.y >= rainEnd) {
+      this.y = this.endy - 100;
+    } else {
+      this.y = this.y + this.velocity;
+    }
+    this.draw();
+  };
+}
+
+const rainArray = [];
+
+for (let i = 0; i < 140; i++) {
+  const rainXLocation = randomNum(canvas.width, 1);
+  const rainYLocation = Math.random() * -500;
+  const randomRainHeight = randomNum(10, 2);
+  const randomSpeed = randomNum(20, 2); // Поправил параметры скорости
+  const randomOpacity = Math.random() * 0.55;
+  rainArray.push(
+    new RainDrops(
+      rainXLocation,
+      rainYLocation,
+      randomRainHeight,
+      randomSpeed,
+      randomOpacity
+    )
+  );
+}
+
+function animateRain() {
+  requestAnimationFrame(animateRain);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = 0; i < rainArray.length; i++) {
+    rainArray[i].update();
+  }
+}
+
+animateRain();
